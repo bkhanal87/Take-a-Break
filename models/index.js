7063ab2af32899eb76ab8adc7f044c38508a2c11
Christian Tanicala <christian.tanicala@gmail.com>
2021-10-30 10:56:13 -0400

    Created foreign key relations in models/index.js

--- models/index.js
+++ models/index.js
@@ -3,4 +3,22 @@ const Category = require('./Category');
 const Food = require('./Food');
 const Review = require('./Review');
 
+// Creating relations, Categories can have multiple foods, but foods have one category
+Category.hasMany(Food, {
+    foreignKey: 'category_id'
+});
+
+Food.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+
+// Creating relations, Users can have multiple reviews, but reviews can have one user
+User.hasMany(Review, {
+    foreignKey: 'user_id'
+});
+
+Review.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Category, Food, Review };
