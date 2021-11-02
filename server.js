d8eaa244636b561443cbcd93f76ea52d5b88b102
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-11-02 18:50:07 -0400

    updating recent changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 
@@ -13,7 +13,7 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
 // // Configure and link a session object with the sequelize store
 // const sess = {
@@ -27,9 +27,9 @@ const hbs = exphbs.create({ helpers });
 // };
 
 // Add express-session and store as Express.js middleware
-app.use(session(sess));
+// app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
@@ -39,5 +39,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}....`));
 });
